bf446e59de00eeb2eb3cd207282d53c090629d3f
michelle <michelle.graham@dkit.ie>
2026-09-15 15:31:23 +0100

    feat: Revision_A1b - sample solution

--- solutions/revision/dictionary_revision.py
+++ solutions/revision/dictionary_revision.py
@@ -9,3 +9,9 @@ if __name__ == "__main__":
     for i in range(len(students)):
         print(f"Student name: {students[i]["name"]}")
         print(f"Average score: {students[i]["average_mark"]}")
+
+    for student_dict in students:
+        if student_dict["name"] == "Dan":
+            print(f"Dan's average mark: {student_dict["average_mark"]}")
+
+    print(f"Dan's average mark: {students[3]["average_mark"]}")
