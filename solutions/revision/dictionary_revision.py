2b13942dbc64aa311903e8fb7358fd95751d11c6
michelle <michelle.graham@dkit.ie>
2026-09-20 01:18:44 +0100

    docs: add labels to solutions for section A in revision exercise sheet

--- solutions/revision/dictionary_revision.py
+++ solutions/revision/dictionary_revision.py
@@ -6,20 +6,26 @@ if __name__ == "__main__":
         {"name": "Dan", "average_mark": 47}
     ]
 
+    # Answer for A1.1:
     for i in range(len(students)):
         print(f"Student name: {students[i]["name"]}")
         print(f"Average score: {students[i]["average_mark"]}")
 
+    # Answer(s) for A1.2:
+    # If we don't know where "Dan" is:
     for student_dict in students:
         if student_dict["name"] == "Dan":
             print(f"Dan's average mark: {student_dict["average_mark"]}")
 
+    # If we know which position "Dan" is in:
     print(f"Dan's average mark: {students[3]["average_mark"]}")
 
+    # Answer for A2:
     for student in students:
         if student["average_mark"] >= 50:
             print(student["name"])
 
+    # Answer for A3:
     max_mark = -1
     name = None
     for i in range(len(students)):
