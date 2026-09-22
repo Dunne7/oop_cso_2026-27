666bd842be4adc5b552e40a05653651131c366bc
Dunne7 <D00288376@student.dkit.ie>
2026-09-22 14:54:28 +0100

    updated file

--- c_student.py
+++ c_student.py
@@ -4,8 +4,13 @@
 students = [
  {"name": "Evan", "marks": [65, 72, 81]},
  {"name": "Caleb", "marks": [45, 51, 48]},
- {"name": "Angelo", "marks": [82, 77, 91]},
+ {"name": "Angelo", "marks": [82,  77, 91]},
  {"name": "Dorothy", "marks": [55, 63, 59]}
 ]
 
 def add_mark(student_dict, new_mark):
+  add_mark(students[1], 66)
+  add_mark(students[2], 77)
+
+print(students)
+
